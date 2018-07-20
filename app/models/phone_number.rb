b6ce725771f15aa0a9fc9760c9c10999999d32fa
Lucas Wolford <lucas.wolford@gmail.com>
2018-07-19 23:33:27 -0400

    Add validation for phone number

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,3 +1,7 @@
 class PhoneNumber < ApplicationRecord
   belongs_to :lead
+
+  validates_format_of :number,
+      :with => /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
+      :message => "- must be in xxx-xxx-xxxx format."
 end
